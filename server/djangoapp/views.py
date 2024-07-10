bef74a4b5519bdb5247d5a97432ca3984afc0186
aomwankhede <wankhede.aom@gmail.com>
2024-07-10 20:35:17 +0530

    user management added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -39,13 +39,28 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName":""}
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+def registration(request):
+    data = json.loads(request.body)
+    firstName = data["firstName"]
+    lastName = data["lastName"]
+    userName = data["userName"]
+    password = data["password"]
+    email = data["email"]
+    try:
+        user = User.objects.create_user(username=userName, email=email, password=password, first_name=firstName, last_name=lastName)
+        user.save()
+        data = {"userName": userName, "status": "Registered"}
+        return JsonResponse(data)
+    except Exception as e:
+        data = {"userName": "", "status": "Registration Failed"}
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
