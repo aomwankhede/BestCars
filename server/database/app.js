b4d4bd0b4f8ee8c2c19e7eb1c9add70352eed5a9
aomwankhede <wankhede.aom@gmail.com>
2024-07-12 03:24:22 +0530

    carMake and carModels added

--- server/database/app.js
+++ server/database/app.js
@@ -8,27 +8,29 @@ const port = 3030;
 app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
-const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
+const reviews_data = JSON.parse(fs.readFileSync("./data/reviews.json", 'utf8'));
+const dealerships_data = JSON.parse(fs.readFileSync("./data/dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+mongoose.connect("mongodb://127.0.0.1:27017",{'dbName':'dealershipsDB'}).then(()=>{
+  console.log("Connected Successfully")
+  try {
+    Reviews.deleteMany({}).then(()=>{
+      Reviews.insertMany(reviews_data['reviews']);
+    });
+    Dealerships.deleteMany({}).then(()=>{
+      Dealerships.insertMany(dealerships_data['dealerships']);
+    });
+    
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+}).catch((err)=>console.log(err));
 
 
 const Reviews = require('./review');
 
 const Dealerships = require('./dealership');
 
-try {
-  Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
-  });
-  Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
-  });
-  
-} catch (error) {
-  res.status(500).json({ error: 'Error fetching documents' });
-}
 
 
 // Express route to home
@@ -58,17 +60,32 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find({state:req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find({id:req.params.id});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
 
 //Express route to insert review
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,17 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -13,7 +19,14 @@
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, choices=[('Sedan', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'WAGON')])
+    year = models.IntegerField(validators=[MaxValueValidator(2023), MinValueValidator(2015)])
 
+    def __str__(self):
+        return self.name
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
